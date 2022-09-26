a2332fd4c6f454ef5259e2b32bce3cfb9c7e26f1
led Ice <Andrey-ssk98@yandex.ru>
2022-09-27 01:28:40 +0300

    ex51

---
+++ HomeWork51/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, columns];
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+    }
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + " ");
+    };
+    Console.WriteLine();
+}
+
+for(int j = 0; j < array.GetLength(1); j++)
+{
+    int sum = 0;
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = array[i,j] + sum;
+    }
+    Console.Write($"Среднее арифметическое каждого столбца: {sum};");
+}
