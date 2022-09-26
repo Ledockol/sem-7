b84c5fbd1b5bd6dd66f24d6a611176507c29bc2b
led Ice <Andrey-ssk98@yandex.ru>
2022-09-27 01:26:24 +0300

    ex47

---
+++ HomeWork47/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[rows, columns];
+
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+    }
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + " ");
+    };
+    Console.WriteLine();
+}
